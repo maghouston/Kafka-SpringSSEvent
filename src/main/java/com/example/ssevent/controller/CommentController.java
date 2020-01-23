dd3e5ba10e25e0d9c005ded9c2c1946c29d835b6
Magloire <mnamber.prestataire@oui.sncf>
2020-01-23 16:56:35 +0100

    nettoyage

--- src/main/java/com/example/ssevent/controller/CommentController.java
+++ src/main/java/com/example/ssevent/controller/CommentController.java
@@ -1,17 +1,11 @@
 package com.example.ssevent.controller;
 
 import com.example.ssevent.EventConsumer;
-import com.example.ssevent.model.Comment;
-import com.example.ssevent.repo.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-
-import java.util.Scanner;
 
 @RestController
 public class CommentController {
--- src/main/java/com/example/ssevent/repo/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ssevent.repo;
-
-import com.example.ssevent.model.Comment;
-import reactor.core.publisher.Flux;
-
-public interface CommentRepository {
-    Flux<Comment> findAll();
-}
